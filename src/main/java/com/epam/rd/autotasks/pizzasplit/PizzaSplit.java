b8856cb9b70b0546b73be7b92f833cef2f514dee
Daniil Laureanti <daniil.laureanti@gmail.com>
2021-11-25 18:01:42 +0200

    Completed tasks

--- src/main/java/com/epam/rd/autotasks/pizzasplit/PizzaSplit.java
+++ src/main/java/com/epam/rd/autotasks/pizzasplit/PizzaSplit.java
@@ -1,11 +1,22 @@
 package com.epam.rd.autotasks.pizzasplit;
 
+import java.util.Scanner;
+
 public class PizzaSplit {
     public static void main(String[] args) {
-        //Write a program, reading number of people and number of pieces per pizza and then
-        //printing minimum number of pizzas to order to split all the pizzas equally and with no remainder
-
-
+        Scanner scanner = new Scanner(System.in);
+        int people = scanner.nextInt();
+        int parts = scanner.nextInt();
+        int i = 1;
+        int find;
+        while (true) {
+            find = i * parts;
+            if (find % people == 0) {
+                break;
+            }
+            i++;
+        }
+        System.out.println(find / parts);
 
     }
 }
